6157f21d4d86bc2ace4513a4f88b73fb87a18d0b
Alex Via <alexandervia@gmail.com>
2023-03-29 12:54:59 -0500

    installs react-router, react-router-dom, adds sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
+import SignUp from './SignUp'
 
 function App() {
   const [properties, setProperties] = useState([])
@@ -16,6 +18,7 @@ function App() {
     }, []
   );
 
+  // Save info as user is logging in
   function handleChange(event) {
     const {name, value} = event.target
       setUser(prevUser => {
@@ -27,6 +30,7 @@ function App() {
     );
   };
 
+  // Log in user 
   function handleSubmit(e) {
     e.preventDefault();
     console.log("signing in")
@@ -37,23 +41,24 @@ function App() {
       console.log(response.data);
       setIsLoggedIn(true);
     })
-  }
+  };
 
-
+  // format or properties shown on page
   const propertyDisplay = properties.map(property => {
     return(
       <div key={property.id}>
-        <h1>{property.title}</h1>
-        <h1>{property.address}</h1>
-        <h1>{property.description}</h1>
+        <h2>{property.title}</h2>
+        <h4>{property.address}</h4>
+        <h4>{property.description}</h4>
         <hr/>
       </div>
     )
-  })
+  });
 
   return (
     <div className="App">
       <header className="App-header">
+        {/* Login form for current users */}
         <form className='login_form' onSubmit={handleSubmit}>
           <input
             type="text"
@@ -71,9 +76,17 @@ function App() {
           /><br/>
           <button className="login_btn" type="submit">Login</button>
         </form>
+        <Link
+          to="/signup"
+          element={<SignUp />}
+        >
+          Sign up here
+        </Link>
+        {/* Temporary placeholder, later displauy username when logged in */}
         <h1>{isLoggedIn ? "User logged in" : "Nobody logged in"}</h1>
         <hr/>
         <hr/>
+        {/* Properties to rent */}
         {propertyDisplay}
       </header>
     </div>
---
+++ src/SignUp.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+export default function SignUp() {
+  const [newUser, setNewUser] = useState(
+    {user_name: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    password_confirmation: ""
+  });
+
+  function handleChange(event) {
+    const {name, value} = event.target
+      setNewUser(prevNewUser => {
+          return {
+            ...prevNewUser,
+            [name] : value
+          }
+      }
+    );
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("signing in")
+    axios.post("http://localhost:3000/users", newUser)
+    .then((response) => {
+      console.log(response.data);
+    })
+  };
+
+  return(
+    <form className='login_form' onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={newUser.user_name}
+            placeholder="Username"
+            name="user_name"
+            onChange={handleChange}
+          /><br/>
+          <input
+            type="text"
+            value={newUser.first_name}
+            placeholder="First Name"
+            name="first_name"
+            onChange={handleChange}
+          /><br/>
+          <input
+            type="text"
+            value={newUser.last_name}
+            placeholder="Last Name"
+            name="last_name"
+            onChange={handleChange}
+          /><br/>
+          <input
+            type="text"
+            value={newUser.email}
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          /><br/>
+          <input
+            type="password"
+            value={newUser.password}
+            placeholder="password"
+            name="password"
+            onChange={handleChange}
+          /><br/>
+          <input
+            type="text"
+            value={newUser.password_confirmation}
+            placeholder="Confirm Password"
+            name="password_confirmation"
+            onChange={handleChange}
+          /><br/>
+          <button className="login_btn" type="submit">Sign up</button>
+        </form>
+  )
+}
